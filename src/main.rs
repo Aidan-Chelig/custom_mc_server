9c187f51b1f527baf436feee892598c4f69f49f9
Aidan Chelig <aidan.chelig@gmail.com>
2022-10-16 17:26:52 -0500

    Initialize repo

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,36 @@
-use std::mem;
+use std::borrow::Cow;
+use std::f64::consts::TAU;
 use std::net::SocketAddr;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use num::Integer;
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
-use valence::biome::Biome;
-use valence::block::BlockState;
-use valence::chunk::{Chunk, UnloadedChunk};
-use valence::client::{handle_event_default, ClientEvent, Hand};
-use valence::config::{Config, ServerListPing};
-use valence::dimension::{Dimension, DimensionId};
-use valence::entity::types::Pose;
-use valence::entity::{EntityId, EntityKind, TrackedData};
+use log::LevelFilter;
+use uuid::Uuid;
+use valence::async_trait;
+use valence::block::{BlockPos, BlockState};
+use valence::chunk::UnloadedChunk;
+use valence::client::{handle_event_default, GameMode};
+use valence::config::{Config, PlayerSampleEntry, ServerListPing};
+use valence::dimension::DimensionId;
+use valence::entity::{EntityId, EntityKind};
 use valence::player_list::PlayerListId;
-use valence::protocol::packets::s2c::play::SoundCategory;
 use valence::server::{Server, SharedServer, ShutdownResult};
 use valence::text::{Color, TextFormat};
-use valence::{async_trait, ident};
-use valence::vek::Vec3;
+use valence::util::to_yaw_and_pitch;
+use vek::{Mat3, Vec3};
 
 pub fn main() -> ShutdownResult {
+    env_logger::Builder::new()
+        .filter_module("valence", LevelFilter::Trace)
+        .parse_default_env()
+        .init();
+
     valence::start_server(
         Game {
             player_count: AtomicUsize::new(0),
         },
         ServerState {
             player_list: None,
-            paused: false,
-            board: vec![false; SIZE_X * SIZE_Z].into_boxed_slice(),
-            board_buf: vec![false; SIZE_X * SIZE_Z].into_boxed_slice(),
+            cows: Vec::new(),
         },
     )
 }
@@ -39,26 +41,17 @@ struct Game {
 
 struct ServerState {
     player_list: Option<PlayerListId>,
-    paused: bool,
-    board: Box<[bool]>,
-    board_buf: Box<[bool]>,
-}
-
-#[derive(Default)]
-struct ClientState {
-    entity_id: EntityId,
+    cows: Vec<EntityId>,
 }
 
 const MAX_PLAYERS: usize = 10;
 
-const SIZE_X: usize = 100;
-const SIZE_Z: usize = 100;
-const BOARD_Y: i32 = 50;
+const SPAWN_POS: BlockPos = BlockPos::new(0, 100, -25);
 
 #[async_trait]
 impl Config for Game {
     type ServerState = ServerState;
-    type ClientState = ClientState;
+    type ClientState = EntityId;
     type EntityState = ();
     type WorldState = ();
     type ChunkState = ();
@@ -69,59 +62,55 @@ impl Config for Game {
         MAX_PLAYERS + 64
     }
 
-    fn dimensions(&self) -> Vec<Dimension> {
-        vec![Dimension {
-            fixed_time: Some(6000),
-            ..Dimension::default()
-        }]
-    }
-
-    fn biomes(&self) -> Vec<Biome> {
-        vec![Biome {
-            name: ident!("valence:default_biome"),
-            grass_color: Some(0x00ff00),
-            ..Biome::default()
-        }]
-    }
-
     async fn server_list_ping(
         &self,
         _server: &SharedServer<Self>,
         _remote_addr: SocketAddr,
         _protocol_version: i32,
     ) -> ServerListPing {
+        const SAMPLE: &[PlayerSampleEntry] = &[
+            PlayerSampleEntry {
+                name: Cow::Borrowed("§cFirst Entry"),
+                id: Uuid::nil(),
+            },
+            PlayerSampleEntry {
+                name: Cow::Borrowed("§6§oSecond Entry"),
+                id: Uuid::nil(),
+            },
+        ];
+
         ServerListPing::Respond {
             online_players: self.player_count.load(Ordering::SeqCst) as i32,
             max_players: MAX_PLAYERS as i32,
-            player_sample: Default::default(),
+            player_sample: SAMPLE.into(),
             description: "Hello Valence!".color(Color::AQUA),
             favicon_png: Some(include_bytes!("../assets/logo-64x64.png").as_slice().into()),
         }
     }
 
     fn init(&self, server: &mut Server<Self>) {
-        let world = server.worlds.insert(DimensionId::default(), ()).1;
+        let (world_id, world) = server.worlds.insert(DimensionId::default(), ());
         server.state.player_list = Some(server.player_lists.insert(()).0);
 
-        for chunk_z in -2..Integer::div_ceil(&(SIZE_Z as i32), &16) + 2 {
-            for chunk_x in -2..Integer::div_ceil(&(SIZE_X as i32), &16) + 2 {
-                world.chunks.insert(
-                    [chunk_x as i32, chunk_z as i32],
-                    UnloadedChunk::default(),
-                    (),
-                );
+        let size = 5;
+        for z in -size..size {
+            for x in -size..size {
+                world.chunks.insert([x, z], UnloadedChunk::default(), ());
             }
         }
+
+        world.chunks.set_block_state(SPAWN_POS, BlockState::BEDROCK);
+        world.chunks.set_block_state((SPAWN_POS.x + 1, SPAWN_POS.y, SPAWN_POS.z), BlockState::BEDROCK);
+
+        server.state.cows.extend((0..200).map(|_| {
+            let (id, e) = server.entities.insert(EntityKind::Cow, ());
+            e.set_world(world_id);
+            id
+        }));
     }
 
     fn update(&self, server: &mut Server<Self>) {
-        let (world_id, world) = server.worlds.iter_mut().next().unwrap();
-
-        let spawn_pos = [
-            SIZE_X as f64 / 2.0,
-            BOARD_Y as f64 + 1.0,
-            SIZE_Z as f64 / 2.0,
-        ];
+        let (world_id, _) = server.worlds.iter_mut().next().expect("missing world");
 
         server.clients.retain(|_, client| {
             if client.created_this_tick() {
@@ -140,7 +129,7 @@ impl Config for Game {
                     .entities
                     .insert_with_uuid(EntityKind::Player, client.uuid(), ())
                 {
-                    Some((id, _)) => client.state.entity_id = id,
+                    Some((id, _)) => client.state = id,
                     None => {
                         client.disconnect("Conflicting UUID");
                         return false;
@@ -149,7 +138,16 @@ impl Config for Game {
 
                 client.spawn(world_id);
                 client.set_flat(true);
-                client.teleport(spawn_pos, 0.0, 0.0);
+                client.set_game_mode(GameMode::Creative);
+                client.teleport(
+                    [
+                        SPAWN_POS.x as f64 + 0.5,
+                        SPAWN_POS.y as f64 + 1.0,
+                        SPAWN_POS.z as f64 + 0.5,
+                    ],
+                    0.0,
+                    0.0,
+                );
                 client.set_player_list(server.state.player_list.clone());
 
                 if let Some(id) = &server.state.player_list {
@@ -162,141 +160,81 @@ impl Config for Game {
                         None,
                     );
                 }
-
-                client.send_message("Welcome to Conway's game of life in Minecraft!".italic());
-                client.send_message(
-                    "Sneak and hold the left mouse button to bring blocks to life.".italic(),
-                );
             }
 
             if client.is_disconnected() {
                 self.player_count.fetch_sub(1, Ordering::SeqCst);
-                server.entities.remove(client.state.entity_id);
                 if let Some(id) = &server.state.player_list {
                     server.player_lists.get_mut(id).remove(client.uuid());
                 }
+                server.entities.remove(client.state);
+
                 return false;
             }
 
-            let player = server.entities.get_mut(client.state.entity_id).unwrap();
+            let entity = server
+                .entities
+                .get_mut(client.state)
+                .expect("missing player entity");
 
-            if client.position().y <= 0.0 {
-                client.teleport(spawn_pos, client.yaw(), client.pitch());
-                server.state.board.fill(false);
-            }
-
-            while let Some(event) = handle_event_default(client, player) {
-                match event {
-                    ClientEvent::Digging { position, .. } => {
-                        if (0..SIZE_X as i32).contains(&position.x)
-                            && (0..SIZE_Z as i32).contains(&position.z)
-                            && position.y == BOARD_Y
-                        {
-                            let index = position.x as usize + position.z as usize * SIZE_X;
-
-                            if !server.state.board[index] {
-                                client.play_sound(
-                                    ident!("minecraft:block.note_block.banjo"),
-                                    SoundCategory::Block,
-                                    Vec3::<i32>::from(position).as_(),
-                                    0.5f32,
-                                    1f32,
-                                );
-                            }
-
-                            server.state.board[index] = true;
-                        }
-                    }
-                    ClientEvent::InteractWithBlock { hand, .. } => {
-                        if hand == Hand::Main {
-                            client.send_message("I said left click, not right click!".italic());
-                        }
-                    }
-                    _ => {}
-                }
-            }
-
-            if let TrackedData::Player(data) = player.data() {
-                let sneaking = data.get_pose() == Pose::Sneaking;
-                if sneaking != server.state.paused {
-                    server.state.paused = sneaking;
-                    client.play_sound(
-                        ident!("block.note_block.pling"),
-                        SoundCategory::Block,
-                        client.position(),
-                        0.5f32,
-                        if sneaking { 0.5f32 } else { 1f32 },
-                    );
-                }
-            }
-
-            // Display Playing in green or Paused in red
-            client.set_action_bar(if server.state.paused {
-                "Paused".color(Color::RED)
-            } else {
-                "Playing".color(Color::GREEN)
-            });
+            while handle_event_default(client, entity).is_some() {}
 
             true
         });
 
-        if !server.state.paused && server.shared.current_tick() % 2 == 0 {
-            server
-                .state
-                .board_buf
-                .par_iter_mut()
-                .enumerate()
-                .for_each(|(i, cell)| {
-                    let cx = (i % SIZE_X) as i32;
-                    let cz = (i / SIZE_Z) as i32;
+        let time = server.shared.current_tick() as f64 / server.shared.tick_rate() as f64;
 
-                    let mut live_count = 0;
-                    for z in cz - 1..=cz + 1 {
-                        for x in cx - 1..=cx + 1 {
-                            if !(x == cx && z == cz) {
-                                let i = x.rem_euclid(SIZE_X as i32) as usize
-                                    + z.rem_euclid(SIZE_Z as i32) as usize * SIZE_X;
-                                if server.state.board[i] {
-                                    live_count += 1;
-                                }
-                            }
-                        }
-                    }
+        let rot = Mat3::rotation_x(time * TAU * 0.1)
+            .rotated_y(time * TAU * 0.2)
+            .rotated_z(time * TAU * 0.3);
 
-                    if server.state.board[cx as usize + cz as usize * SIZE_X] {
-                        *cell = (2..=3).contains(&live_count);
-                    } else {
-                        *cell = live_count == 3;
-                    }
-                });
+        let radius = 6.0 + ((time * TAU / 2.5).sin() + 1.0) / 2.0 * 10.0;
 
-            mem::swap(&mut server.state.board, &mut server.state.board_buf);
-        }
+        let player_pos = server
+            .clients
+            .iter()
+            .next()
+            .map(|c| c.1.position())
+            .unwrap_or_default();
 
-        let min_y = server.shared.dimensions().next().unwrap().1.min_y;
+        // TODO: remove hardcoded eye pos.
+        let eye_pos = Vec3::new(player_pos.x, player_pos.y + 1.6, player_pos.z);
 
-        for chunk_x in 0..Integer::div_ceil(&SIZE_X, &16) {
-            for chunk_z in 0..Integer::div_ceil(&SIZE_Z, &16) {
-                let chunk = world
-                    .chunks
-                    .get_mut((chunk_x as i32, chunk_z as i32))
-                    .unwrap();
-                for x in 0..16 {
-                    for z in 0..16 {
-                        let cell_x = chunk_x * 16 + x;
-                        let cell_z = chunk_z * 16 + z;
+        for (cow_id, p) in server
+            .state
+            .cows
+            .iter()
+            .cloned()
+            .zip(fibonacci_spiral(server.state.cows.len()))
+        {
+            let cow = server.entities.get_mut(cow_id).expect("missing cow");
+            let rotated = p * rot;
+            let transformed = rotated * radius + [0.5, SPAWN_POS.y as f64 + 2.0, 0.5];
 
-                        if cell_x < SIZE_X && cell_z < SIZE_Z {
-                            let b = if server.state.board[cell_x + cell_z * SIZE_X] {
-                                BlockState::GRASS_BLOCK
-                            } else {
-                                BlockState::DIRT
-                            };
-                            chunk.set_block_state(x, (BOARD_Y - min_y) as usize, z, b);
-                        }
-                    }
-                }
-            }
+            let yaw = f32::atan2(rotated.z as f32, rotated.x as f32).to_degrees() - 90.0;
+            let (looking_yaw, looking_pitch) =
+                to_yaw_and_pitch((eye_pos - transformed).normalized());
+
+            cow.set_position(transformed);
+            cow.set_yaw(yaw);
+            cow.set_pitch(looking_pitch as f32);
+            cow.set_head_yaw(looking_yaw as f32);
         }
     }
+}
+
+/// Distributes N points on the surface of a unit sphere.
+fn fibonacci_spiral(n: usize) -> impl Iterator<Item = Vec3<f64>> {
+    (0..n).map(move |i| {
+        let golden_ratio = (1.0 + 5_f64.sqrt()) / 2.0;
+
+        // Map to unit square
+        let x = i as f64 / golden_ratio % 1.0;
+        let y = i as f64 / n as f64;
+
+        // Map from unit square to unit sphere.
+        let theta = x * TAU;
+        let phi = (1.0 - 2.0 * y).acos();
+        Vec3::new(theta.cos() * phi.sin(), theta.sin() * phi.sin(), phi.cos())
+    })
 }
